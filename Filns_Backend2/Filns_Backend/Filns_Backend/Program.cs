254d4123488c72f7a1bdd96fd7d6b3f4a48945ef
s185132 <s185132@student.dtu.dk>
2021-11-27 15:38:35 +0000

    made dev branch

--- Filns_Backend2/Filns_Backend/Filns_Backend/Program.cs
+++ Filns_Backend2/Filns_Backend/Filns_Backend/Program.cs
@@ -9,33 +9,12 @@ namespace Filns_Backend
 {
     public class Program
     {
-     /*   /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-        */
-
-
 
         WordReader WordReader = new WordReader();
         WordGenerator WordGenerator = new WordGenerator();
         DataSheetReader DataSheetReader = new DataSheetReader();
         DataSheetReader2 DataSheetReader2 = new DataSheetReader2();
         List<string> aliasList = new List<string>();
-        //string[] alias = {"<name>", "<firstname>", "<birthday>", "<date>" };
-
-
-        object[] vals = { "Jensen", "Filip", "21-09-98", DateTime.Now };
-        int[] rangeVals = {1, 1, 4, 1};
-
-
 
         public Program(){
             
@@ -44,8 +23,11 @@ namespace Filns_Backend
         public IData CreateDocument(IData data){
 
 
+            
             aliasList = WordReader.ReadAlias(data.document.filepath);
             string[] alias2 = aliasList.ToArray();
+
+
             WordGenerator.CreateWordDocument(
                 data.document.filepath,
                 data.resultDoc.filepath,
