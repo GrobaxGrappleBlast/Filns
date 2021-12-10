d742c806fd05e157ec2e9dde1649d4144755d892
s185132 <s185132@student.dtu.dk>
2021-12-10 14:01:07 +0100

    added WPF closeButton to Repo

---
+++ WPF_CloseButton/WPF_CloseButton/ClippingBorder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CloseButton {
+    // Source https://stackoverflow.com/questions/324641/how-to-make-the-contents-of-a-round-cornered-border-be-also-round-cornered
+
+    public class ClippingBorder : Border {
+        protected override void OnRender(DrawingContext dc) {
+            OnApplyChildClip();
+            base.OnRender(dc);
+        }
+
+        public override UIElement Child {
+            get {
+                return base.Child;
+            }
+            set {
+                if(this.Child != value) {
+                    if(this.Child != null) {
+                        // Restore original clipping
+                        this.Child.SetValue(UIElement.ClipProperty , _oldClip);
+                    }
+
+                    if(value != null) {
+                        _oldClip = value.ReadLocalValue(UIElement.ClipProperty);
+                    } else {
+                        // If we dont set it to null we could leak a Geometry object
+                        _oldClip = null;
+                    }
+
+                    base.Child = value;
+                }
+            }
+        }
+
+        protected virtual void OnApplyChildClip() {
+            UIElement child = this.Child;
+            if(child != null) {
+                _clipRect.RadiusX = _clipRect.RadiusY = Math.Max(0.0 , this.CornerRadius.TopLeft - (this.BorderThickness.Left * 0.5));
+                _clipRect.Rect = new Rect(Child.RenderSize);
+                child.Clip = _clipRect;
+            }
+        }
+
+        private RectangleGeometry _clipRect = new RectangleGeometry();
+        private object _oldClip;
+    }
+}
